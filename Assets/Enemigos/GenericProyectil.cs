6936944571f2c107ae7d601b9ec497113ed702a2
lenof <lenof_@outlook.com>
2023-05-11 18:07:00 -0500

    Script de balas genéricas - Fabrizio Sánchez

---
+++ Assets/Enemigos/GenericProyectil.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericProyectil : MonoBehaviour
+{
+    [SerializeField] PlayerStats pStats;
+    [SerializeField] float damage;
+    [SerializeField] Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        pStats = GameObject.Find("Player").GetComponent<PlayerStats>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            MakeDamage();
+        }
+        Destroy(this.gameObject);
+    }
+
+    public void MakeDamage()
+    {
+        pStats._pLife -= damage;
+    }
+}
