e4c56d735b3c7d4d53a091a1908eb7c83ae3969d
Piero Mazzino <pieromazzino@MacBook-Pro-de-Piero.local>
2023-05-11 19:24:00 -0500

    Enemigo 1 Bala 1 movimiento disparo Piero Mazzino

---
+++ Assets/Enemigos/Enemigo1/BalaE1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaE1 : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 2f);
+
+    }
+}
